0fd75b64e1e7f953ba9726b5ebd3eabb1c8f55b0
fing9 <djajsl54@naver.com>
2024-07-31 14:31:56 +0900

    [Chore]: 최초 프로젝트 세팅

---
+++ src/main/java/com/dahhong/whoami/WhoamiApplication.java
@@ -0,0 +1,13 @@
+package com.dahhong.whoami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahhong/whoami/WhoamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahhong.whoami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
