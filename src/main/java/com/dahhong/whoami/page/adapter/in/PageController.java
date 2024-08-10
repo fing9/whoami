71f01b89e90e6b39152e0436da8f33ed794c8acd
sungpaks <emforhs03150@gmail.com>
2024-08-10 18:11:23 +0900

    [update] TODO 구현 : create, delete, update, get 메서드

--- src/main/java/com/dahhong/whoami/page/adapter/in/PageController.java
+++ src/main/java/com/dahhong/whoami/page/adapter/in/PageController.java
@@ -2,7 +2,14 @@ package com.dahhong.whoami.page.adapter.in;
 
 import com.dahhong.whoami.global.response.ApiResponse;
 import com.dahhong.whoami.page.adapter.in.dto.CreatePageRequestDto;
+import com.dahhong.whoami.page.adapter.in.dto.CreatePageResponseDto;
 import com.dahhong.whoami.page.adapter.in.swagger.PageControllerSwagger;
+import com.dahhong.whoami.page.application.port.in.CreatePageUseCase;
+import com.dahhong.whoami.page.application.port.in.DeletePageUseCase;
+import com.dahhong.whoami.page.application.port.in.GetPageUseCase;
+import com.dahhong.whoami.page.application.port.in.UpdatePageUseCase;
+import com.dahhong.whoami.page.application.service.DeletePageService;
+import com.dahhong.whoami.page.application.service.UpdatePageService;
 import com.dahhong.whoami.page.domain.entity.Page;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,43 +20,44 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/page")
 public class PageController implements PageControllerSwagger {
 
+	private final CreatePageUseCase createPageUseCase;
+
+	private final DeletePageUseCase deletePageUseCase;
+
+	private final UpdatePageUseCase updatePageUseCase;
+
+	private final GetPageUseCase getPageUseCase;
+	private final DeletePageService deletePageService;
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> getPage(@PathVariable Long id) {
-		/**
-		 * TODO: 로직
-		 */
-		return ResponseEntity.ok(ApiResponse.success(/* TODO: data! */));
+		Page page = getPageUseCase.getPage(id);
+		return ResponseEntity.ok(ApiResponse.success(page));
 	}
 
 	@GetMapping("/all")
 	public ResponseEntity<?> getAllPages() {
 		/**
-		 * TODO: 로직
+		 * TODO : 로직
 		 */
 		return ResponseEntity.ok(ApiResponse.success(/* TODO: data! */));
 	}
 
 	@PostMapping("/create")
 	public ResponseEntity<?> createPage(@RequestBody CreatePageRequestDto createPageRequest) {
-		/**
-		 * TODO: 로직
-		 */
-		return ResponseEntity.ok(ApiResponse.success(/* data? */));
+		Page createdPage = createPageUseCase.createPage(createPageRequest.getUserId(), createPageRequest.getTitle());
+		return ResponseEntity.ok(ApiResponse.success(new CreatePageResponseDto("성공적으로 페이지를 생성했습니다", createdPage.getId())));
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> updatePage(@PathVariable Long id, @RequestBody Page pageDetails) {
-		/**
-		 * TODO : 로직
-		 */
+		updatePageUseCase.updatePage(pageDetails);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deletePage(@PathVariable Long id) {
-		/**
-		 * TODO : 로직
-		 */
+		deletePageService.deletePage(id);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 }
