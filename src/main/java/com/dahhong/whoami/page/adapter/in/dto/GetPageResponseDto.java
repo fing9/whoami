b44cb280632e9b49a7bd90e251af89db78af41af
sungpaks <emforhs03150@gmail.com>
2024-08-13 22:37:12 +0900

    [feat] 응답DTO에 created date, modified date 추가

--- src/main/java/com/dahhong/whoami/page/adapter/in/dto/GetPageResponseDto.java
+++ src/main/java/com/dahhong/whoami/page/adapter/in/dto/GetPageResponseDto.java
@@ -5,18 +5,24 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetPageResponseDto {
 	private Long id;
 	private String title;
 	private String userId;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
 
 	public static GetPageResponseDto of (Page page) {
 		return new GetPageResponseDto(
 				page.getId(),
 				page.getTitle(),
-				page.getUser().getId()
+				page.getUser().getId(),
+				page.getCreatedDate(),
+				page.getModifiedDate()
 		);
 	}
 }
