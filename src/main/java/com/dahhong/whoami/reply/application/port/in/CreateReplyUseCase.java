84ce2dfacdd684648c4a67e15a31831632f4e532
sungpaks <emforhs03150@gmail.com>
2024-08-27 17:18:05 +0900

    [fix] Reply Create UseCase 인자 수정

--- src/main/java/com/dahhong/whoami/reply/application/port/in/CreateReplyUseCase.java
+++ src/main/java/com/dahhong/whoami/reply/application/port/in/CreateReplyUseCase.java
@@ -3,5 +3,5 @@ package com.dahhong.whoami.reply.application.port.in;
 import com.dahhong.whoami.reply.domain.entity.Reply;
 
 public interface CreateReplyUseCase {
-	Reply createReply(String replyUserId, String content);
+	Reply createReply(Long pageId, String replyUsername, String content);
 }
