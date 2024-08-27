1dc20016d3bbe190a60069487fc6515ed4620a7a
sungpaks <emforhs03150@gmail.com>
2024-08-27 16:49:26 +0900

    [update] Reply 레포지토리 메서드 추가

--- src/main/java/com/dahhong/whoami/reply/infrastructure/repository/ReplyRepository.java
+++ src/main/java/com/dahhong/whoami/reply/infrastructure/repository/ReplyRepository.java
@@ -3,6 +3,7 @@ package com.dahhong.whoami.reply.infrastructure.repository;
 import com.dahhong.whoami.reply.domain.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
 	@Query("select r from Reply r join fetch r.page ")
 	List<Reply> findAllJoinFetch();
+
+	@Query("select r from Reply r join fetch r.page p where p.id = :pageId")
+	List<Reply> findByPageId(Long pageId);
 }
