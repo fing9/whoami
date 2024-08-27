e22d4a3727bdb287543678619de6e1aa5d7070d4
sungpaks <emforhs03150@gmail.com>
2024-08-27 16:24:02 +0900

    [feat] Reply 엔티티 추가

---
+++ src/main/java/com/dahhong/whoami/reply/domain/entity/Reply.java
@@ -0,0 +1,25 @@
+package com.dahhong.whoami.reply.domain.entity;
+
+import com.dahhong.whoami.global.auditing.BaseTimeEntity;
+import com.dahhong.whoami.page.domain.entity.Page;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Table(name = "wai_reply")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Reply extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name = "page_id")
+	private Page page;
+
+	@Column(length = 500)
+	private String content;
+}
